0f09db44e8cdb68c792f7318564aef40ca836baa
antoineatrhea <a.theate@rheagroup.com>
2023-10-03 15:33:03 +0200

    Fix #1: project architecture

---
+++ CDP4.COMET.HERMES/Pages/Error.cshtml.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Error.cshtml.cs" company="RHEA System S.A.">
+// 
+//    Copyright (c) 2023 RHEA System S.A.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4.COMET.HERMES.Pages
+{
+    using System.Diagnostics;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    /// <summary>
+    /// Data model that provide information about request when an error occurs
+    /// </summary>
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
+    {
+        /// <summary>
+        /// Gets or sets the request id
+        /// </summary>
+        public string RequestId { get; set; }
+
+        /// <summary>
+        /// Gets the assert the the <see cref="RequestId" /> should be shown
+        /// </summary>
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
+
+        /// <summary>
+        /// Sets the <see cref="RequestId" /> based on context
+        /// </summary>
+        public void OnGet()
+        {
+            this.RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier;
+        }
+    }
+}
